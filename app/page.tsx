4f62c9425a01c87b0f014b81ad856de897b02f5c
Notsaumitra <saumitra.1997.fcb@gmail.com>
2023-08-05 19:17:24 +0530

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
+import Footer from './components/Footer'
 
 export default function Home() {
   return (
     <main className='text-center'>
       <section>
-      <Image className='rounded-full' width={500} height={500} src={"/ONE4ESPORTS.jpg"} alt='logo_One4Esports'/>
+        <div className="flex justify-center">
+        <Image className='rounded-full' width={500} height={500} src={"/ONE4ESPORTS.jpg"} alt='logo_One4Esports'/>
+        </div>
       <p className='text-2xl font-black text-blue-600'>Coming Soon...</p>
       </section>
+      <Footer />
     </main>
   )
 }
